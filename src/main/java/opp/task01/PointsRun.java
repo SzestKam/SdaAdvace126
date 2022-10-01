798c69a766ad3f29bac0371e44c49aec61a466de
developer <developer@java.pl>
2022-10-01 11:32:02 +0200

    zadanie nr 1 z sekcji OPP

---
+++ src/main/java/opp/task01/PointsRun.java
@@ -0,0 +1,15 @@
+package opp.task01;
+
+class PointsRun {
+
+  public static void main(String[] args) {
+    Point2D _2d = new Point2D(3, 4);
+    Point3D _3d_a = new Point3D(22, 33, 44);
+    Point2D _3d_b = new Point3D(220, 330, 440);
+
+    System.out.println("2D: " + _2d);
+    System.out.println("3D: " + _3d_a);
+    System.out.println("3D: " + _3d_b);
+  }
+
+}
